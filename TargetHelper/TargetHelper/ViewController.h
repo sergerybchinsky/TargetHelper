1d4d04460f54c8b2d92b570626ce6e3b47f3d7c5
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-09-17 15:11:42 +0700

    Created obj-c project.

---
+++ TargetHelper/TargetHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TargetHelper
+//
+//  Created by SergeRybchinsky on 17/09/16.
+//  Copyright © 2016 SergeRybchinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
